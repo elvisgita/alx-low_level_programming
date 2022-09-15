697cd20d240ee70b35689e4aef4eadf74a2a40d7
elvisgita <elvisgita@gmail.com>
2022-09-15 13:37:54 -0700

    first task

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers ho here */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n -rand() - RAND_MAX / 2;
+	/* code goes here */
+	return (0);
